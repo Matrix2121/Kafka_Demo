fa42862d95b5e359bbdddd9dce3e7928990538f2
Matrix2121 <vasko10.vs38@gmail.com>
2025-11-26 01:12:44 +0200

    quick fixes for correct start of consumers

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -7,10 +7,10 @@ class Program
     static void Main(string[] args)
     {
         Env.Load();
-        //_service.Start().GetAwaiter().GetResult();
+        //analitycs_service.Start().GetAwaiter().GetResult();
         //prices_data_warehouse_archiver.Start().GetAwaiter().GetResult();
         //realtime_prices_stream_ui.Start().GetAwaiter().GetResult();
-        //notification_sender_worker.Start().GetAwaiter().GetResult();
+        //notification_sender_engine.Start().GetAwaiter().GetResult();
         //order_execution_engine.Start().GetAwaiter().GetResult();
         //ledger_service.Start().GetAwaiter().GetResult();
     }
