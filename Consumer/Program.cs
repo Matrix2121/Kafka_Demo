91f9c6c7daf7ae9e5cb94b63f4d47a02b7dd543d
Matrix2121 <vasko10.vs38@gmail.com>
2025-11-26 23:44:50 +0200

    fix naming bug

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -7,7 +7,7 @@ class Program
     static void Main(string[] args)
     {
         Env.Load();
-        //analitycs_service.Start().GetAwaiter().GetResult();
+        //analytics_service.Start().GetAwaiter().GetResult();
         //prices_data_warehouse_archiver.Start().GetAwaiter().GetResult();
         //realtime_prices_stream_ui.Start().GetAwaiter().GetResult();
         //notification_sender_engine.Start().GetAwaiter().GetResult();
